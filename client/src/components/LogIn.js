e3fd8ade308c857b63f7041a4fb8b7b787b2c5cb
daniel <daniel.wamsher@gmail.com>
2022-06-09 13:58:53 -0700

    dotenv

---
+++ client/src/components/LogIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Jwt from Jwt;
+
+const LogIn = () => {
+  return (
+    <div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default LogIn
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-// const = require('')
+const JWT = require('jsonwebtoken')
 // const = require('')
 
 const PORT = 5000;
